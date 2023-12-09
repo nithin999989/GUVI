75834bf8696fc13ce34a8e03f633398e3c83b81e
M N S R NITHIN <82808965+nithin999989@users.noreply.github.com>
2023-12-09 07:04:43 +0530

    Update register.php

--- php/register.php
+++ php/register.php
@@ -1,6 +1,9 @@
 <?php
 
     require 'connect.inc.php';
+      $mongoDbUrl = "mongodb+srv://mnsrnithin9:<password>@userprofile.49uw1sb.mongodb.net/?retryWrites=true&w=majority";
+    $mongoDbName = "userdata";
+    $collectionName = "profiledata";
     if(isset( $_POST['firstName']) && isset( $_POST['lastName']) && isset( $_POST['email']) && isset( $_POST['password'])&& isset( $_POST['confirmPassword'])&& isset( $_POST['dob'])&& isset( $_POST['mobileNumber']))
     {
         $firstName =  $_POST['firstName'];
@@ -10,6 +13,22 @@
         $confirmPassword =  $_POST['confirmPassword'];
         $dob =  $_POST['dob'];
         $mobileNumber =  $_POST['mobileNumber'];
+
+        
+        $client = new MongoDB\Client($mongoDbUrl);
+        $database = $client->$mongoDbName;
+        $collection = $database->$collectionName;
+          $dataToInsert = [
+            [   "fullname"    => $fullname,
+                "DOB"         => $dob,
+                "Current"     => $current_location,
+                "mail"        => $mail,
+                "passkey"     => $passkey,
+                "contact"     => $phone
+
+              ]
+        ];
+         $collection->insertMany($dataToInsert);
 
         $query_signup = "INSERT INTO `users` VALUES ('', '$firstName', '$lastName', '$email', '$password', '$dob', '$mobileNumber')";
         mysqli_query( $con, $query_signup );
