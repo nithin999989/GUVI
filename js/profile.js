8e06e39fe4318e979168a067bb03987150945280
M N S R NITHIN <82808965+nithin999989@users.noreply.github.com>
2023-12-09 06:47:20 +0530

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -25,7 +25,23 @@ function updateProfile() {
 
     // Add more profile details updating as needed
 }
-
+function check() {
+    $.ajax({
+        url: 'http://localhost/vendor/redis.php',
+        method: 'GET',
+        success: function(response) {
+            if (response.data === true) {
+                window.location.href = 'profile.html';
+            } else {
+                window.location.href = 'login.html';
+            }
+        },
+        error: function(error) {
+            console.error('Error:', error);
+            window.location.href = 'login.html';
+        }
+    });
+}
 // Call the updateProfile function when the page loads
 window.onload = function () {
     updateProfile();
